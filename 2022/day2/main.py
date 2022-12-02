dd14cde13524d06d5fb76de0c8dd58b2e65a337b
Alok <alok.dhondt@proton.me>
2022-12-02 23:44:17 +1300

    started working on the problem

--- 2022/day2/main.py
+++ 2022/day2/main.py
@@ -1,10 +1,11 @@
 # The mapping between moves and numbers
-opponent = {0: 'A', # Rock
-            1: 'B', # Paper
-            2: 'C'} # Scissors
-me = {0: 'X', # Rock
-      1: 'Y', # Paper
-      2: 'Z'} # Scissors
+opponent = {'A': 0, # Rock
+            'B': 1, # Paper
+            'C': 2} # Scissors
+
+me = {'X': 0, # Rock
+      'Y': 1, # Paper
+      'Z': 2} # Scissors
 
 scores = {'A': 1,
           'B': 2,
@@ -14,9 +15,13 @@ scores = {'A': 1,
           'Z': 3}
 
 # Win-lose matrix
-win_lose = [[-1, 1, 0],
-            [1, -1, 2],
-            [0, 2, -1]]
+win_lose = [[-1,  1,  0], # Rock
+            [ 1, -1,  2], # Paper
+            [ 0,  2, -1]] # Scissors
+            #Roc Pap Sci
+points = {"Lose": 0,
+          "Draw": 3,
+          "Win": 6}
 
 # read given input into python
 with open('input.txt') as input_text:
@@ -27,3 +32,7 @@ input_text.close()
 
 # Part 1
 # What would your total score be if everything goes exactly according to your strategy guide?
+final_score = 0
+for g in game :
+    result = win_lose[opponent.get(g[0])][me.get(g[1])]
+    print(result)
