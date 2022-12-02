e508de899a6b9cfa47f7e4341062025c3f64c5dd
Alok <alok.dhondt@proton.me>
2022-12-03 00:40:39 +1300

    part 1 complete

--- 2022/day2/main.py
+++ 2022/day2/main.py
@@ -1,11 +1,10 @@
 # The mapping between moves and numbers
-opponent = {'A': 0, # Rock
-            'B': 1, # Paper
-            'C': 2} # Scissors
-
-me = {'X': 0, # Rock
-      'Y': 1, # Paper
-      'Z': 2} # Scissors
+key = { 'A': 0, # Rock
+        'B': 1, # Paper
+        'C': 2, # Scissors
+        'X': 0, # Rock
+        'Y': 1, # Paper
+        'Z': 2} # Scissors
 
 scores = {'A': 1,
           'B': 2,
@@ -19,9 +18,6 @@ win_lose = [[-1,  1,  0], # Rock
             [ 1, -1,  2], # Paper
             [ 0,  2, -1]] # Scissors
             #Roc Pap Sci
-points = {"Lose": 0,
-          "Draw": 3,
-          "Win": 6}
 
 # read given input into python
 with open('input.txt') as input_text:
@@ -34,5 +30,18 @@ input_text.close()
 # What would your total score be if everything goes exactly according to your strategy guide?
 final_score = 0
 for g in game :
-    result = win_lose[opponent.get(g[0])][me.get(g[1])]
-    print(result)
+    op = g[0]
+    me = g[1]
+    result = win_lose[key.get(op)][key.get(me)]
+    score = scores.get(me)
+    if result == key.get(me):
+        score += 6
+    elif result == -1:
+        score += 3
+    print(score)
+    final_score += score
+
+print(final_score)
+
+# Part 2
+# What would your total score be if everything goes exactly according to your strategy guide?
