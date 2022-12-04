606ce4059a6e9765389a3f577761e9aec00f9c6f
Alok <alok.dhondt@proton.me>
2022-12-04 17:22:46 +1300

    part 1 cleaned working on part 2

--- 2022/day2/main.py
+++ 2022/day2/main.py
@@ -14,14 +14,7 @@ class Outcome(Enum):
     Draw = 3
 
 
-key = {'A': 0,  # Rock
-       'B': 1,  # Paper
-       'C': 2,  # Scissors
-       'X': 0,  # Rock
-       'Y': 1,  # Paper
-       'Z': 2}  # Scissors
-
-scores = {'A': 1,
+values = {'A': 1,
           'B': 2,
           'C': 3,
           'X': 1,
@@ -32,95 +25,80 @@ outcomes = {'X': 0,
             'Y': 3,
             'Z': 6}
 
-# Win-lose matrix
-win_lose = [[-1, 1, 0],  # Rock [0,0] [0,1], [0,2]
-            [1, -1, 2],  # Paper
-            [0, 2, -1]]  # Scissors
-#           Roc Pap Sci
 
-# win lose array
-win_lose_array = [0, 1, 2]
+def parse_input(data_file: str) -> list[(Move, Move)]:
+    with open(data_file) as input_text:
+        moves = [pair.strip().split() for pair in input_text.readlines()]
+        return [(Move(values[a]), Move(values[b]), Outcome(outcomes[b])) for a, b in moves]
 
-# read given input into python
-with open('input.txt') as input_text:
-    raw = input_text.readlines()
-    game = [pair.strip().split(' ') for pair in raw]
-    print(game)
-input_text.close()
+
+def determine_winner(a: Move, b: Move) -> Outcome:
+    if a == b:
+        return Outcome.Draw
+    else:
+        return Outcome.Lose if Move(((a.value + 1) % 3) + 1) == b else Outcome.Win
+
 
 # Part 1
 # What would your total score be if everything goes exactly according to your strategy guide?
-final_score = 0
-for g in game:
-    op = g[0]
-    me = g[1]
-    result = win_lose[key[op]][key[me]]
-    score = scores[me]
-    if result == key[me]:
-        score += Outcome.Win.value
-    elif result == -1:
-        score += Outcome.Draw.value
-    print(score)
-    final_score += score
+def part_one(moves: list[(Move, Move, Outcome)]) -> int:
+    ans = 0
+    for a, b, c in moves:
+        ans += determine_winner(a, b).value + b.value
+    return ans
 
-print(final_score)
 
-final_score = 0
-for g in game:
-    move = g[0]
-    required_outcome = g[1]
-    opponents_play = Move(scores[move])
-    print(opponents_play)
-    my_outcome = Outcome(outcomes[required_outcome])
-    print(my_outcome)
-    final_score += my_outcome.value
-    match my_outcome:
+def determine_play(a: Move, c: Outcome) -> Move:
+    match c:
         case Outcome.Draw:
-            final_score += opponents_play.value
+            return a
         case Outcome.Win:
-            results = win_lose[key[move]]
-            for i, j in enumerate(results):
-                if i == j:
-                    final_score += i+1
-                    break
+            return Move(((a.value + 1) % 3) + 1)
         case Outcome.Lose:
-            results = win_lose_array[(key[move] - 1) % 3]
-            print(key[move])
-            print(results)
-            final_score += results + 1
-    print(final_score)
+            return Move(((a.value - 1) % 3) + 1)
 
-
-
-
-
-#def b_wins(a: Move, b: Move) -> Outcome:
-#    if a == b:
-#        return Outcome.Draw
-#    return Outcome.Lose if Move(((a.value + 1) % 3) + 1) == b else Outcome.Win
-#
-#
-#def play(game: list[(Move, Move)]) -> int:
-#    score = 0
-#    for a, b in game:
-#        score += b_wins(a, b).value + b.value
-#    return score
-#
-#
-#def parse_input(data_file: str) -> list[(Move, Move)]:
-#    with open(data_file) as f:
-#        game_raw = [pair.strip().split() for pair in f.readlines()]
-#        return [(Move(scores[a]), Move(scores[b])) for a, b in game_raw]
-#
-#
-#def main():
-#    game = parse_input('input.txt')
-#    print(f"Part 1: {play(game)}")
-#
-#
-#if __name__ == "__main__":
-#    main()
-#    raise SystemExit(0)
 
 # Part 2
 # What would your total score be if everything goes exactly according to your strategy guide?
+def part_two(moves: list[(Move, Move, Outcome)]) -> int:
+    ans = 0
+    for a, b, c in moves:
+        print(f"play {determine_play(a, c).value + c.value}")
+        ans += determine_play(a, c).value + c.value
+    return ans
+
+
+# final_score = 0
+# for g in game:
+#     move = g[0]
+#     required_outcome = g[1]
+#     opponents_play = Move(values[move])
+#     print(opponents_play)
+#     my_outcome = Outcome(outcomes[required_outcome])
+#     print(my_outcome)
+#     final_score += my_outcome.value
+#     match my_outcome:
+#         case Outcome.Draw:
+#             final_score += opponents_play.value
+#         case Outcome.Win:
+#             results = win_lose[key[move]]
+#             for i, j in enumerate(results):
+#                 if i == j:
+#                     final_score += i + 1
+#                     break
+#         case Outcome.Lose:
+#             results = win_lose_array[(key[move] - 1) % 3]
+#             print(key[move])
+#             print(results)
+#             final_score += results + 1
+#     print(final_score)
+
+def main():
+    inputs = parse_input('input.txt')
+    print(f"Part 1: {part_one(inputs)}")
+    print(f"Part 2: {part_two(inputs)}")
+
+
+if __name__ == "__main__":
+    main()
+    raise SystemExit(0)
