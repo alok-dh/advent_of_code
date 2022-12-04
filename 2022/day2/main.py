35198e0c709fc91d3932a70bd2d669391a184c50
Alok <alok.dhondt@proton.me>
2022-12-04 17:50:41 +1300

    updated part 2 - using methods because jake showed me the way

--- 2022/day2/main.py
+++ 2022/day2/main.py
@@ -2,6 +2,9 @@
 from enum import Enum
 
 
+# Move Enum where hierarchy can be determined by:
+#   (Move.value % 3) + 1 beats Move
+#   (Move.vale + 1)  % 3) + 1 looses to Move
 class Move(Enum):
     Rock = 1
     Paper = 2
@@ -26,7 +29,8 @@ outcomes = {'X': 0,
             'Z': 6}
 
 
-def parse_input(data_file: str) -> list[(Move, Move)]:
+# Return triples of Move a, Move b, and Part 2's Outcome
+def parse_input(data_file: str) -> list[(Move, Move, Outcome)]:
     with open(data_file) as input_text:
         moves = [pair.strip().split() for pair in input_text.readlines()]
         return [(Move(values[a]), Move(values[b]), Outcome(outcomes[b])) for a, b in moves]
@@ -52,10 +56,10 @@ def determine_play(a: Move, c: Outcome) -> Move:
     match c:
         case Outcome.Draw:
             return a
-        case Outcome.Win:
-            return Move(((a.value + 1) % 3) + 1)
-        case Outcome.Lose:
-            return Move(((a.value - 1) % 3) + 1)
+        case Outcome.Win:  # Return the move required to win
+            return Move((a.value % 3) + 1)
+        case Outcome.Lose:  # Return the move required to lose
+            return Move(((a.value + 1) % 3) + 1)  #
 
 
 # Part 2
@@ -63,35 +67,9 @@ def determine_play(a: Move, c: Outcome) -> Move:
 def part_two(moves: list[(Move, Move, Outcome)]) -> int:
     ans = 0
     for a, b, c in moves:
-        print(f"play {determine_play(a, c).value + c.value}")
         ans += determine_play(a, c).value + c.value
     return ans
 
-
-# final_score = 0
-# for g in game:
-#     move = g[0]
-#     required_outcome = g[1]
-#     opponents_play = Move(values[move])
-#     print(opponents_play)
-#     my_outcome = Outcome(outcomes[required_outcome])
-#     print(my_outcome)
-#     final_score += my_outcome.value
-#     match my_outcome:
-#         case Outcome.Draw:
-#             final_score += opponents_play.value
-#         case Outcome.Win:
-#             results = win_lose[key[move]]
-#             for i, j in enumerate(results):
-#                 if i == j:
-#                     final_score += i + 1
-#                     break
-#         case Outcome.Lose:
-#             results = win_lose_array[(key[move] - 1) % 3]
-#             print(key[move])
-#             print(results)
-#             final_score += results + 1
-#     print(final_score)
 
 def main():
     inputs = parse_input('input.txt')
