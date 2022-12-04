18fbec11bc766cb938d13fbbb415bf8584b05239
Alok <alok.dhondt@proton.me>
2022-12-04 13:46:11 +1300

    part 2 done also

--- 2022/day3/main.py
+++ 2022/day3/main.py
@@ -3,9 +3,11 @@
 with open('input.txt') as input_text:
     raw = input_text.readlines()
     rucksack = [[item for item in items if item != '\n'] for items in raw]
+#   make my containers tuples of sets to remove duplicate items in common
     containers = [[set(contents[len(contents) // 2:]), set(contents[:len(contents) // 2])] for contents in rucksack]
     print(containers)
 
+# Goes from 0-52 used for scoring priority via invoking the .index function
 priority = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # Part 1
@@ -18,5 +20,16 @@ for container in containers:
 
 print(sum_of_priority)
 
-
-
+# Part 2
+# Find the item type that corresponds to the badges of each three-Elf group.
+# What is the sum of the priorities of those item types?
+# I don't want the split sets from part one
+containers_part_two = [set(contents) for contents in rucksack]
+i = 0
+sum_of_badges = 0
+while i < len(containers_part_two):
+    # taking advantage of intersect to peak into all 3 sets in one go
+    badge = containers_part_two[i].intersection(containers_part_two[i+1], containers_part_two[i+2])
+    sum_of_badges += priority.index(badge.pop())
+    i += 3
+print(sum_of_badges)
