dcec789dbe9e4fff1975206700ba066d820a34ab
Alok <alok.dhondt@proton.me>
2022-12-04 17:53:46 +1300

    using fstrings

--- 2022/day3/main.py
+++ 2022/day3/main.py
@@ -1,11 +1,9 @@
-
 # read given input into python
 with open('input.txt') as input_text:
     raw = input_text.readlines()
     rucksack = [[item for item in items if item != '\n'] for items in raw]
-#   make my containers tuples of sets to remove duplicate items in common
+    # make my containers tuples of sets to remove duplicate items in common
     containers = [[set(contents[len(contents) // 2:]), set(contents[:len(contents) // 2])] for contents in rucksack]
-    print(containers)
 
 # Goes from 0-52 used for scoring priority via invoking the .index function
 priority = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -18,7 +16,7 @@ for container in containers:
     common_item = container[0].intersection(container[1])
     sum_of_priority += priority.index(common_item.pop())
 
-print(sum_of_priority)
+print(f"Part 1: {sum_of_priority}")
 
 # Part 2
 # Find the item type that corresponds to the badges of each three-Elf group.
@@ -32,4 +30,4 @@ while i < len(containers_part_two):
     badge = containers_part_two[i].intersection(containers_part_two[i+1], containers_part_two[i+2])
     sum_of_badges += priority.index(badge.pop())
     i += 3
-print(sum_of_badges)
+print(f"Part 2: {sum_of_badges}")
