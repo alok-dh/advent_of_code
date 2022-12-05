64432076fe1ebe61377e30b3bfe6458563731052
Alok <alok.dhondt@proton.me>
2022-12-05 22:01:37 +1300

    made part 2, but should update to run both in one go

--- 2022/day5/main.py
+++ 2022/day5/main.py
@@ -1,26 +1,25 @@
 # read given input into python
 with open('input.txt') as input_text:
     crates_diagram, moves = [text.strip().split("\n") for text in input_text.read().split("\n\n")]
-    # moves = moves_text.splitlines()
-    print(crates_diagram)
 
 # Part 1
 # After the rearrangement procedure completes,
 # what crate ends up on top of each stack?
+# Part 2
+# Before the rearrangement process finishes,
+# update your simulation so that the Elves know where
+# they should stand to be ready to unload the final supplies.
+# After the rearrangement procedure completes,
+# what crate ends up on top of each stack?
 crates = [[] for _ in crates_diagram[0][1::4]]
-for row in crates_diagram[-2::-1]:  # flip and remove index number
-    print(row)
+for row in crates_diagram[-2::-1]:  # flip and remove index number very cool inspired by reddit
     for i, crate in enumerate(row[1::4]):
         if crate != " ":
             crates[i].append(crate)
-print(crates)
 
-instrctions = []
 for move in moves:
     _, x_many, _, from_y, _, to_z = move.split()
-    instrctions.append([int(x_many), int(from_y), int(to_z)])
-print(instrctions)
-
-
-
-
+    for i in range(int(x_many)):
+        # crates[int(to_z) - 1].append(crates[int(from_y) - 1].pop())  # part 1
+        crates[int(to_z) - 1].insert(len(crates[int(to_z) - 1]), crates[int(from_y) - 1].pop())  # part 2
+print("".join(c[-1] for c in crates))
