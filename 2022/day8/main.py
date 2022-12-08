cd3cc6820a8792acb90e16f46bcc4283d73a3c9e
Alok <alok.dhondt@proton.me>
2022-12-08 18:23:01 +1300

    day 8 test off by one

--- 2022/day8/main.py
+++ 2022/day8/main.py
@@ -1,4 +1,31 @@
 # read given input into python
 with open('test.txt') as input_text:
-    trees = [text.strip() for text in input_text.readlines()]
+    trees = [[tree for tree in text.strip()] for text in input_text.readlines()]
     print(trees)
+
+# Part 1
+# Consider your map; how many trees are visible from outside the grid?
+rows = len(trees[0])
+cols = len(trees)
+visible = rows*2 + (cols-2)*2
+print(visible)
+for i in range(1, cols):
+    for j in range(1, rows):
+        tree = trees[i][j]
+        if tree > trees[i-1][j]:
+            print(tree)
+            visible += 1
+            break
+        elif tree > trees[i+1][j]:
+            print(tree)
+            visible += 1
+            break
+        elif tree > trees[i][j-1]:
+            print(tree)
+            visible += 1
+            break
+        elif tree > trees[i][j+1]:
+            print(tree)
+            visible += 1
+            break
+print(visible)
