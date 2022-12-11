1065d875fd04edcfc01c595f9890c3bccf946ed6
Alok <alok.dhondt@proton.me>
2022-12-11 21:35:49 +1300

    worked on test but not on input

--- 2022/day11/main.py
+++ 2022/day11/main.py
@@ -1,9 +1,7 @@
-import operator
 import re
-from collections import defaultdict
 
 # read given input into python
-with open('test.txt') as input_text:
+with open('input.txt') as input_text:
     text = [[m.strip() for m in line.split('\n')] for line in input_text.read().split('\n\n')]
     print(text)
 
@@ -15,17 +13,21 @@ class Operation:
         self.operation = operation
 
     def do_operation(self, old):
+        x = self.x
         if self.x == 'old':
-            self.x = old
+            x = old
         match self.operation:
             case '+':
-                return operator.add(old, int(self.x))
+                return old + int(x)
             case '-':
-                return operator.sub(old, int(self.x))
+                return old - int(x)
             case '*':
-                return operator.mul(old, int(self.x))
+                return old * int(x)
             case '/':
-                return operator.truediv(old, int(self.x))
+                return old // int(x)
+
+    def __str__(self):
+        return f"{self.operation} {self.x}"
 
 
 class Monkey:
@@ -35,21 +37,23 @@ class Monkey:
         self.items = items
         self.operation = operation
         self.test = test
-        self.f = f
         self.t = t
-
-    def __str__(self):
-        return f"Monkey: {self.number}, Items: {self.items}"
+        self.f = f
+        self.inspected = 0
 
     def update_items(self, item):
         self.items.append(item)
 
+    def inspect(self):
+        self.inspected += 1
+
 
 # construct a monke
-monkeys = [Monkey for i in range(len(text))]
+monkeys: list[Monkey] = [Monkey for i in range(len(text))]
 for monke in text:
     number, items, operation, test, t, f = None, None, None, None, None, None
     for m in monke:
+        print(m)
         match m.strip(':').split():
             case 'Monkey', num:
                 number = int(num)
@@ -65,23 +69,25 @@ for monke in text:
                 items = [int(i) for i in re.findall('\d\d', m)]
     monkeys[number] = Monkey(number, items, operation, test, t, f)
 
-for monkey in monkeys:
-    print(monkey)
+# for monkey in monkeys:
+#     print(monkey.__dict__)
+#     print(monkey.operation)
 
-for i in range(20):
+for _ in range(20):
     for m in monkeys:
-        if len(m.items) == 0: continue
-        worry = m.items.pop(0)
-        worry = m.operation.do_operation(worry)
-        worry = worry // 3
-        if worry // m.test:
-            monkeys[m.t].items.append(worry)
-        else:
-            monkeys[m.f].items.append(worry)
+        while True:
+            if len(m.items) == 0: break
+            worry = m.items.pop(0)
+            m.inspect()
+            worry = m.operation.do_operation(worry)
+            worry = worry // 3
+            if worry % m.test == 0:
+                monkeys[m.t].update_items(worry)
+            else:
+                monkeys[m.f].update_items(worry)
 
-for monkey in monkeys:
-    print(monkey.items)
-
-
+active_monkeys = sorted([m.inspected for m in monkeys])
+print(active_monkeys)
+print(active_monkeys[-2] * active_monkeys[-1])
 
 
