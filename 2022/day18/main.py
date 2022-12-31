e0b3a860e33b910c18a92e84f409f7bb3151813b
Alok <alok.dhondt@proton.me>
2022-12-31 17:49:57 +1300

    part 2 works on input now

--- 2022/day18/main.py
+++ 2022/day18/main.py
@@ -1,25 +1,28 @@
 with open("input.txt") as f:
-    text = [[int(l) for l in line.strip().split(',')] for line in f.readlines()]
-    print(text)
+    cubes = [[int(l) for l in line.strip().split(',')] for line in f.readlines()]
+    print(cubes)
 
 # Part 1
 # What is the surface area of your scanned lava droplet?
 area = 0
-for x, y, z in text:
+for x, y, z in cubes:
     for p, q, s, in ((x + 1, y, z), (x - 1, y, z), (x, y + 1, z), (x, y - 1, z), (x, y, z + 1), (x, y, z - 1)):
-        if [p, q, s] not in text: area += 1
+        if [p, q, s] not in cubes: area += 1
 print(area)
 
 # Part 2
 # What is the exterior surface area of your scanned lava droplet?
-area = 0
-for x, y, z in text:
+exterior_area = 0
+seen = set()
+queue = [(0, 0, 0)]
+while queue:
+    x, y, z = queue.pop()
+    seen.add((x, y, z))
     for p, q, s, in ((x + 1, y, z), (x - 1, y, z), (x, y + 1, z), (x, y - 1, z), (x, y, z + 1), (x, y, z - 1)):
-        if [p, q, s] not in text:
-            area += 1
-            count = 0
-            for x2, y2, z2, in ((p + 1, q, s), (p - 1, q, s), (p, q + 1, s), (p, q - 1, s), (p, q, s + 1), (p, q, s - 1)):
-                if [x2, y2, z2] in text:
-                    count += 1
-            if count == 6: area -= 1
-print(area)
+        if -1 <= p <= 22 and -1 <= q <= 22 and -1 <= z <= 22 \
+                and (p, q, s) not in seen and (p, q, s) not in queue:
+            if [p, q, s] in cubes:
+                exterior_area += 1
+            else:
+                queue.append((p, q, s))
+print(exterior_area)
