99e64be16e9124cdb9e45daf8ec52ad3c20485ce
Alok <alok.dhondt@proton.me>
2023-12-02 18:36:42 +1300

    added julia

--- 2023/day1/day1.jl
+++ 2023/day1/day1.jl
@@ -1,4 +1,20 @@
 input = open("2023\\day1\\input.txt", "r")
-map = read(input, String)
+map = readlines(input)
 close(input)
-println(map)
+println(typeof(map))
+
+for m in map
+    global results
+    first_match = match(r"\d", m)
+    last_match = match(r"\d(?=\D*$)", m)
+    
+    # Check if both matches are not empty
+    if first_match !== nothing && last_match !== nothing
+        num = string(first_match.match, last_match.match)
+        results += parse(Int, num)
+    else
+        println("Skipping line: $m")
+    end
+end
+
+print(results)
